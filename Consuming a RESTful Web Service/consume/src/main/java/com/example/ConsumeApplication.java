ed6da98b3779020b0606d15803e530a069ab66aa
zhuoyikang <zhuoyikang@gmail.com>
2016-08-03 21:39:20 +0800

    xs1

---
+++ Consuming a RESTful Web Service/consume/src/main/java/com/example/ConsumeApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumeApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumeApplication.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(ConsumeApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+}
