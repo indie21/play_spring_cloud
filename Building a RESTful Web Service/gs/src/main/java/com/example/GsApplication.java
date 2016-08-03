0ac6dd4ae7b511157221c8f8711a5fe2ddac682a
zhuoyikang <zhuoyikang@gmail.com>
2016-08-03 21:36:43 +0800

    good

---
+++ Building a RESTful Web Service/gs/src/main/java/com/example/GsApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableEurekaClient
+public class GsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsApplication.class, args);
+    }
+
+}
