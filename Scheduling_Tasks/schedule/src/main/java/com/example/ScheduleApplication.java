f15416bb6a90e03ba3780ba1fa71de82b2af13ec
zhuoyikang <zhuoyikang@gmail.com>
2016-08-20 11:11:14 +0800

    tm[

--- Scheduling_Tasks/schedule/src/main/java/com/example/ScheduleApplication.java
+++ Scheduling_Tasks/schedule/src/main/java/com/example/ScheduleApplication.java
@@ -7,8 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ScheduleApplication {
-
-	public static void main(String[] args) {
-      SpringApplication.run(ScheduleApplication.class, args);
-	}
+        public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
 }
