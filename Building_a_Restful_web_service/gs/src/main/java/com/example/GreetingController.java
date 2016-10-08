e5d0b828427d0ff869d25f490e4dc4c6a7a42ee4
zhuoyikang <zhuoyikang@gmail.com>
2016-10-08 14:58:11 +0800

    new

--- Building_a_Restful_web_service/gs/src/main/java/com/example/GreetingController.java
+++ Building_a_Restful_web_service/gs/src/main/java/com/example/GreetingController.java
@@ -4,17 +4,21 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class GreetingController {
 
+    private static Logger log = LoggerFactory.getLogger(GreetingController.class);
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        log.info(name);
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
 }
