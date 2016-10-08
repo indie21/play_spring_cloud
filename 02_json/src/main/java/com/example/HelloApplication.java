aa575e382d726387dea0cdbbe49c8383b228b2f1
zhuoyikang <zhuoyikang@gmail.com>
2016-10-08 17:46:06 +0800

    02

--- 02_json/src/main/java/com/example/HelloApplication.java
+++ 02_json/src/main/java/com/example/HelloApplication.java
@@ -3,10 +3,8 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class HelloApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
