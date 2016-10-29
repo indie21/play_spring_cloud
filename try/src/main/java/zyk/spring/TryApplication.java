9e1a6d96380ee019e2628dfa8689eb6e32cd1288
zhuoyikang <zhuoyikang@gmail.com>
2016-10-29 23:03:14 +0800

    up

--- try/src/main/java/zyk/spring/TryApplication.java
+++ try/src/main/java/zyk/spring/TryApplication.java
@@ -1,5 +1,6 @@
 package zyk.spring;
 
+
 // import org.springframework.boot.SpringApplication;
 // import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,14 +14,8 @@ package zyk.spring;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class TryApplication {
@@ -50,7 +45,6 @@ public class TryApplication {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj = (HelloWorld)context.getBean("helloWorld");
         obj.getMessage();
-
         context.registerShutdownHook();
 
         HelloIndia obj1 = (HelloIndia)context.getBean("helloIndia");
