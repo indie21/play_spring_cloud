b47c6e6e1eea3f8b712d581923073ac5cf19d728
zhuoyikang <zhuoyikang@gmail.com>
2016-10-31 17:59:48 +0800

    student

--- try/src/main/java/zyk/spring/TryApplication.java
+++ try/src/main/java/zyk/spring/TryApplication.java
@@ -1,67 +1,16 @@
 package zyk.spring;
 
-
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class TryApplication {
-
-// 	public static void main(String[] args) {
-//       SpringApplication.run(TryApplication.class, args);
-// 	}
-// }
-
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class TryApplication {
     public static void main(String[] args) {
-        // ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        // HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        // System.out.printf("object message %s\n", obj.getMessage());
-
-        // XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
-        // HelloWorld obj = (HelloWorld)factory.getBean("helloWorld");
-        // obj.getMessage();
-
-        // XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
-        // HelloWorld obj1 = (HelloWorld)factory.getBean("helloWorld");
-        // obj1.getMessage();
-
-        // obj1.setMessage("good");
-
-        // HelloWorld obj2 = (HelloWorld)factory.getBean("helloWorld");
-        // obj2.getMessage();
-
-        // obj2.setMessage("nice");
-
-        // obj1.getMessage();
-        // obj2.getMessage();
-
-        // AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        // HelloWorld obj = (HelloWorld)context.getBean("helloWorld");
-        // obj.getMessage();
-        // context.registerShutdownHook();
-
-        // HelloIndia obj1 = (HelloIndia)context.getBean("helloIndia");
-        // obj1.getMessage();
-
-
-        // ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        // TextEditor te = (TextEditor)context.getBean("textEditor");
-        // te.spellCheck();
-
-
-        ApplicationContext context =
-            new ClassPathXmlApplicationContext("Beans.xml");
-
-        Student student = (Student) context.getBean("student");
-        System.out.println("Name : " + student.getName() );
-        System.out.println("Age : " + student.getAge() );
-
+        ApplicationContext ctx =
+            new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+        HelloWorld helloWorld = ctx.getBean(HelloWorld.class);
+        helloWorld.setMessage("Hello World!");
+        helloWorld.getMessage();
     }
 }
